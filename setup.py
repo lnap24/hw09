a329e9913b345e9fa66f4eb19fa6b7fa0cf00dca
lnap24 <luke.nap@comcast.net>
2024-07-02 17:30:05 -0400

    updating repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='booklover',
+      version='0.1',
+      description='booklover hw',
+      url='https://github.com/lnap24/hw09',
+      author='LJN',
+      author_email='ljn5yms@virginia.edu',
+      license='MIT',
+      packages=['booklover']) #must be directory name
